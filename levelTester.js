aa9f245e0712eb86655e71b8d63c56f6d2fb017a
PerfectOrphan31 <jmcdo29@gmail.com>
2018-09-21 13:24:01 -0700

    Added a tab character to align the | in the logs.

--- levelTester.js
+++
@@ -1,8 +0,0 @@
-const mcScribe = require('./dist/index');
-
-mcScribe.consoleLogger('FINE', 'Test');
-mcScribe.consoleLogger('DEBUG', 'Test');
-mcScribe.consoleLogger('INFO', 'Test');
-mcScribe.consoleLogger('WARN', 'Test');
-mcScribe.consoleLogger('ERROR', 'Test');
-mcScribe.consoleLogger('FATAL', 'Test');
